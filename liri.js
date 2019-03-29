1b1e69bddf125181f6902941d36ac9ce91780e99
William Baires <williambaires@Williams-MacBook-Pro.local>
2019-03-28 18:04:39 -0700

    first commit

---
+++ liri.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+var keys = require('./keys');
+// var request = require('request');
+// var fs = require('fs');
+
+// var spotify = new Spotify(keys.spotify);
+
+//  search:  function({ type: 'tracks', query: 'My search query', limit: 20 }, callback);
+
+// var Spotify = require('node-spotify-api');
+ 
+// var spotify = new Spotify({
+//   id: <a766f967f4a0471aa3a6e256e1b4920e>,
+//   secret: <38fc047286704878b7985568bdd3bfae>
+// });
+ 
+// spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
